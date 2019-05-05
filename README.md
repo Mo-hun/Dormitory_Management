477f0ac50c4fe24229b496384922516d50d860a1
twiceland1109 <ahgns1515@naver.com>
2019-05-05 23:10:52 +0900

    기초

---
+++ README.md
@@ -0,0 +1,34 @@
+# 벌점왕 HS
+## 광주소프트웨어마이스터고등학교 기숙사 사생 관리 웹사이트
+<hr>
+<br>
+https://img.shields.io/badge/<Website>-<Go>-<brightgreen>.svg 
+<br>
+<hr>
+Client 요구 사항
+회원가입
+로그인
+공구 신청 상황 확인
+공구 신청
+공구 종류 확인
+공지
+관리자 로그인
+관리자 회원가입
+관리자 공구 등록
+관리자 신청 승인
+<br>
+<hr>
+선택적 추가 기능
+게시판 (CS, Notice)
+댓글 (Comment)
+쿠키 사용으로 자동 로그아웃 및 조회수 카운팅
+불량 사용자에 대한 차단 처리
+<br>
+<br>
+<hr>
+
+##Update Log
+<br><br>
+
+Pre-Alpha 1.0
+메인 페이지
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php
+  include_once('./include/session.php');
+ ?>
+<?php
+  include_once('./include/dbconnect.php');
+ ?>
+ <?php
+   include_once('./include/session.php');
+  ?>
+<html lang="ko" dir="ltr">
+  <head>
+    <?php
+      include_once('./include/head.php');
+     ?>
+    <title>벌점왕HS - Main</title>
+  </head>
+  <body>
+     <?php
+      include_once('./include/main.php');
+      ?>
+    <?php
+      include_once('./include/copyright.php');
+     ?>
+     <?php
+       include_once('./include/jquery.php');
+      ?>
+  </body>
+</html>
