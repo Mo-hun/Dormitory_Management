020b9d62736712c48e50dba5fe1c34ebce5b589f
twiceland1109 <ahgns1515@naver.com>
2019-05-06 00:01:05 +0900

    add include

---
+++ include/head.php
@@ -0,0 +1,16 @@
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="벌점왕HS">
+  <meta name="keywords" content="광주소프트웨어마이스터고, 광주소프트웨어마이스터고등학교, GSM, 기숙사, 광주소프트웨어마이스터고등학교기숙사, 광주소프트웨어마이스터고기숙사, 벌점, 벌점관리, 상점, 상점관리, 광주소프트웨어마이스터고벌점, 광주소프트웨어마이스터고등학교벌점,사생관리, 사생관리 웹사이트, 사생, 관리, 상벌점, 벌점왕, 벌점왕HS,">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="벌점왕HS">
+  <meta property="og:description" content="광주소프트웨어마이스터고등학교 사생관리 웹사이트">
+  <meta property="og:image" content="https://www.벌점왕hs.com/logo.jpg">
+  <meta property="og:url" content="https://www.벌점왕hs.com/logo.jpg">
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+  <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+  <link rel="stylesheet" href="./CSS/bootstrap.css">
+  <link rel="stylesheet" href="./CSS/bootstrap-grid.css">
+  <link rel="stylesheet" href="./CSS/bootstrap-reboot.css">
+  <link rel="stylesheet" href="./CSS/main.css">
+  <meta name="naver-site-verification" content="2bf285b30724c485f7ee8236cb5a619ca9d3df6f"/>
