5196785df6672654e33a004a0600cd8e8b6c926d
내맘이네전부를기억해 <ahgns1515@naver.com>
2019-06-02 19:31:19 +0900

    dormitory_inspector left_sidebar

--- include/dormitory_inspector_left_sidebar.php
+++ include/dormitory_inspector_left_sidebar.php
@@ -4,23 +4,20 @@
     <nav>
       <ul class="nav">
         <li><a href="/dormitory_inspector_main" <?php if($location == "main") { ?>class="active" <?php } ?>><i class="lnr lnr-home"></i> <span>메인</span></a></li>
-        <li><a href="/dormitory_inspector_go_point" <?php if($location == "go_point") { ?>class="active" <?php } ?>><i class="lnr lnr-code"></i> <span>상*벌점 부여</span></a></li>
-        <li><a href="/dormitory_inspector_volunteer" class=""><i class="lnr lnr-chart-bars"></i> <span>봉사관리</span></a></li>
-        <li><a href="/dormitory_inspector_student_check" class=""><i class="lnr lnr-cog"></i> <span>점호/인원점검</span></a></li>
-        <li><a href="dormitory_inspector_notice" class=""><i class="lnr lnr-alarm"></i> <span>전달사항</span></a></li>
+        <li><a href="/dormitory_inspector_go_point" <?php else if($location == "go_point") { ?>class="active" <?php } ?>><i class="lnr lnr-magic-wand"></i> <span>상*벌점 부여</span></a></li>
+        <li><a href="/dormitory_inspector_volunteer" <?php else if($location == "volunteer") { ?>class="active" <?php } ?>><i class="lnr lnr-star-empty"></i> <span>봉사관리</span></a></li>
+        <li><a href="/dormitory_inspector_student_check" <?php else if($location == "student_check") { ?>class="active" <?php } ?>><i class="lnr lnr-spell-check"></i> <span>점호/인원점검</span></a></li>
+        <li><a href="/dormitory_inspector_notice" <?php else if($location == "notice") { ?>class="active" <?php } ?>><i class="lnr lnr-bullhorn"></i> <span>전달사항</span></a></li>
         <li>
-          <a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Pages</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-          <div id="subPages" class="collapse ">
+          <a href="#subPages" data-toggle="collapse" class="collapsed<?php else if($location == "change_room" || $location == "change_year" || $location == "mypage") { echo 'active" aria-expanded="true'; ?>"><i class="lnr lnr-pencil"></i> <span>정보변경</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
+          <div id="subPages"class="collapse <?php if($location == "change_room" || $location == "change_year" || $location == "mypage") {echo "in";}?>">
             <ul class="nav">
-              <li><a href="page-profile.html" class="">Profile</a></li>
-              <li><a href="page-login.html" class="">Login</a></li>
-              <li><a href="page-lockscreen.html" class="">Lockscreen</a></li>
+              <li><a href="/dormitory_inspector_change_room" class="<?php if($location == "change_room"){ echo "active";} ?>">호실 변경</a></li>
+              <li><a href="/dormitory_inspector_change_year" class="<?php else if($location == "change_year"){ echo "active";} ?>">학년 변경</a></li>
+              <li><a href="/dormitory_inspector_mypage" class="<?php else if($location == "mypage"){ echo "active";} ?>">계정 정보 변경</a></li>
             </ul>
           </div>
         </li>
-        <li><a href="tables.html" class=""><i class="lnr lnr-dice"></i> <span>Tables</span></a></li>
-        <li><a href="typography.html" class=""><i class="lnr lnr-text-format"></i> <span>Typography</span></a></li>
-        <li><a href="icons.html" class=""><i class="lnr lnr-linearicons"></i> <span>Icons</span></a></li>
       </ul>
     </nav>
   </div>
