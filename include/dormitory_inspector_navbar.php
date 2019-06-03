af7da8132d6ae7963f97d216da808a13f81f1809
twiceland1109 <ahgns1515@naver.com>
2019-06-03 18:07:06 +0900

    delete weather button

--- include/dormitory_inspector_navbar.php
+++ include/dormitory_inspector_navbar.php
@@ -8,9 +8,8 @@
       <button type="button" class="btn-toggle-fullwidth"><i class="lnr lnr-arrow-left-circle"></i></button>
     </div>
     <div class="navbar-btn navbar-btn-right">
-      <a class="btn btn-success update-pro" href="/logout" title="LOG OUT"><span>LOG OUT</span></a>
-      <a class="btn btn-primary update-pro" href="https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EA%B4%91%EC%82%B0%EA%B5%AC+%EC%86%A1%EC%A0%95+1%EB%8F%99++%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80&oquery=%EA%B4%91%EC%82%B0%EA%B5%AC+%EC%86%A1%EC%A0%95+1%EB%8F%99++%EB%82%A0%EC%94%A8&tqi=UM3Drwp0J14ss48LZSVsssssti0-030322" title="미세먼지" target="_blank"><span>미세먼지</span></a>
-      <a class="btn btn-primary update-pro" href="https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EA%B4%91%EC%82%B0%EA%B5%AC+%EC%86%A1%EC%A0%95+1%EB%8F%99++%EB%82%A0%EC%94%A8&oquery=%EA%B4%91%EC%82%B0%EA%B5%AC+%EC%86%A1%EC%A0%95+1%EB%8F%99+%EB%82%B4%EC%9D%BC+%EB%82%A0%EC%94%A8&tqi=UM3Drlp0JXVssc8CAd8ssssssol-428859" title="날씨" target="_blank"><span>날씨</span></a>
+      <a class="btn btn-info" href="/dormitory_inspector_mypage" title="정보변경"><span>정보변경</span></a>
+      <a class="btn btn-success" href="/logout" title="LOG OUT"><span>LOG OUT</span></a>
     </div>
   </div>
 </nav>
