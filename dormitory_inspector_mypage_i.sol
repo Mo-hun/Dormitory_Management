be789707a4e4f686335c4b128c1aad15c245443f
twiceland1109 <ahgns1515@naver.com>
2019-06-10 08:38:10 +0900

    success mypage form

--- dormitory_inspector_mypage_i.sol
+++ dormitory_inspector_mypage_i.sol
@@ -17,40 +17,37 @@
     <title>벌점왕HS</title>
   </head>
   <body class="bg-dark text-white">
-    <form action="./go_point_i_process"  method="POST">
+    <form action="./dormitory_inspector_mypage_process"  method="POST">
             <div class="form-row">
              <div class="form-group col-md-6">
-               <label for="name">사유</label>
-               <input type="text" class="form-control" id="name" name="name" placeholder="상*벌점 사유">
-               <label for="name">상*벌점 부여 사유를 알려주세요.</label>
-             </div>
-             <div id="display">
+               <label for="id">ID</label>
+               <input type="text" class="form-control" id="id" name="id" value="<?php echo $_SESSION['id']; ?>">
+               <label for="id">현재 사감선생님의 ID 정보 입니다.</label>
              </div>
              <div class="form-group col-md-6">
-               <label for="helpdate">일자</label>
-               <input type="text" class="form-control" id="helpdate" name="date" value="<?php echo date("Y-m-d");?>">
+               <label for="nowpw">현재PW</label>
+               <input type="password" class="form-control" id="nowpw" name="nowpw" placeholder="현재 비밀번호를 입력하세요.">
+               <label for="nowpw">계정 정보를 변경하려면 현재 비밀번호를 입력하세요.</label>
              </div>
              <div class="form-group col-md-6">
-               <label for="helpstudent">대상학생</label>
-               <input type="text" class="form-control" id="helpstudent" name="student" placeholder="동명이인의 경우 뒤에 학번을 입력주세요. ex-김상현/2222">
+               <label for="newpw">변경할PW</label>
+               <input type="text" class="form-control" id="newpw" name="newpw" placeholder="새 비밀번호 (필수 아님)">
+               <label for="newpw">비밀번호를 변경하려면 새로 변경할 비밀번호를 입력하세요.</label>
              </div>
              <div class="form-group col-md-6">
-               <label for="helpscore">점수</label>
-               <input type="text" class="form-control" id="helpscore" name="score" placeholder="부과할 상*벌점의 점수를 입력해주세요.">
+               <label for="newpwCheck">변경할PW 재확인</label>
+               <input type="text" class="form-control" id="newpwCheck" name="newpwCheck" placeholder="새 비밀번호 다시 입력 (필수 아님)">
+               <label for="newpwCheck">비밀번호를 변경하려면 새로 변경할 비밀번호를 다시한번 입력하세요.</label>
              </div>
            </div>
            <div class="custom-control custom-checkbox">
-             <input type="checkbox" class="custom-control-input" id="customCheck1" name="customCheck1" value="1">
-             <label class="custom-control-label" for="customCheck1">상점의 경우 체크해주세요.</label>
-           </div>
-           <div class="custom-control custom-checkbox">
-             <input type="checkbox" class="custom-control-input" id="customCheck2" name="customCheck1" value="2">
-             <label class="custom-control-label" for="customCheck2">벌점의 경우 체크해주세요.</label>
+             <input type="checkbox" class="custom-control-input" id="newpwCheckbox" name="newpwCheckbox" value="1">
+             <label class="custom-control-label" for="newpwCheckbox">비밀번호를 변경할 시 체크</label>
            </div>
            <br>
            <div class="form-group row">
              <div class="col-sm-10">
-               <button type="submit" class="btn btn-primary">상*벌점 부과</button>
+               <button type="submit" class="btn btn-primary">계정 정보 변경하기</button>
              </div>
            </div>
         </form>
