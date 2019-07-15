74e84547dd7ac6cc813154b361e53c9929b9abc1
내맘이네전부를기억해 <ahgns1515@naver.com>
2019-07-16 08:43:44 +0900

    complete Laptop Lent

--- include/student_laptop_is.php
+++ include/student_laptop_is.php
@@ -9,7 +9,7 @@
   if($laptop_no[$laptop_you_no] == 1){
     $str = "<script>var query = confirm('노트북 대여를 하시겠습니까? 이 선택은 아직은 되돌릴수 없으며 실험적입니다.');if(query){window.location.href = '/student_laptop_proc';}else{window.location.href= '/';}</script>";
   }else{
-    $str = "<script>alert('이미 대여 되어 있습니다.');</script>";
+    $str = "<script>alert('이미 대여 되어 있습니다.');window.location.href='/';</script>";
   }
   echo $str;
  ?>
--- include/student_laptop_proc.php
+++ include/student_laptop_proc.php
@@ -6,15 +6,15 @@
   $is_result = mysqli_fetch_assoc($isis_result);
   $hello = $is_result[(string)$class];
   $laptop_no = str_split($hello);
-  $laptop_you_no = $_SESSION['no']+1;
-  if($laptop_no[$laptop_you_no]){
-    $laptop_no[$laptop_you_no] = 0;
+  $laptop_you_no = $_SESSION['number']-1;
+  if($laptop_no[(int)$laptop_you_no]){
+    $laptop_no[(int)$laptop_you_no] = "0";
   }
   $laptop_update = join("",(array)$laptop_no);
   $update_query = "UPDATE laptop SET `".$class_no."`='".$laptop_update."' WHERE status = 1";
   $update_result = mysqli_query($conn, $update_query);
   if($update_result){
-    $str = "<script>alert('성공!');window.location.href='/'</script>";
+    $str = "<script>alert('성공!');window.location.href='/';</script>";
   }else{
     $str =  "<script>alert('실패!');</script>";
   }
